2707dd12e29e0a1e1af4cc6ab577a6f934856e25
forz <73713286+forzzzzz@users.noreply.github.com>
2024-05-08 16:02:08 +0200

    fix crash: when selEnd < selStart

--- app/src/main/java/com/forz/calculator/customViews/ExpressionEditText.kt
+++ app/src/main/java/com/forz/calculator/customViews/ExpressionEditText.kt
@@ -8,11 +8,13 @@ import com.forz.calculator.viewModels.ExpressionViewModel
 class ExpressionEditText(context: Context, attrs: AttributeSet) : AppCompatEditText(context, attrs) {
     override fun onSelectionChanged(selStart: Int, selEnd: Int) {
         super.onSelectionChanged(selStart, selEnd)
-        if ((selStart != ExpressionViewModel.expressionCursorPositionStart.value!! || selEnd != ExpressionViewModel.expressionCursorPositionEnd.value!!) && !ExpressionViewModel.isExpressionInserted){
-            ExpressionViewModel.updateCursorPosition(selStart, selEnd)
-        }
-        if (selStart == ExpressionViewModel.numberOfCharactersOfInsertedText && ExpressionViewModel.isExpressionInserted){
-            ExpressionViewModel.updateIsExpressionInserted(false)
+        if (selEnd > selStart) {
+            if ((selStart != ExpressionViewModel.expressionCursorPositionStart.value!! || selEnd != ExpressionViewModel.expressionCursorPositionEnd.value!!) && !ExpressionViewModel.isExpressionInserted) {
+                ExpressionViewModel.updateCursorPosition(selStart, selEnd)
+            }
+            if (selStart == ExpressionViewModel.numberOfCharactersOfInsertedText && ExpressionViewModel.isExpressionInserted) {
+                ExpressionViewModel.updateIsExpressionInserted(false)
+            }
         }
     }
 }
