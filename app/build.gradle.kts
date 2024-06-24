5ad33a8805bcc1faca034459f0bc5755de4c86a8
forz <73713286+forzzzzz@users.noreply.github.com>
2024-06-24 13:04:31 +0200

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.marktka.calculatorYou"
         minSdk = 27
         targetSdk = 34
-        versionCode = 18
-        versionName = "2.2.1"
+        versionCode = 19
+        versionName = "2.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
