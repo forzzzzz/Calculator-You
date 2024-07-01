a4543f2e86772a18e9ae2685f5042a789afb8342
forz <73713286+forzzzzz@users.noreply.github.com>
2024-07-01 23:52:08 +0200

    changed settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,6 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
-        maven { url = uri("https://redempt.dev")}
     }
 }
 
